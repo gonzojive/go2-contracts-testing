be3b0ed19c9cc0ecc375d34c0c717cb2f00ddd2a
Red Daly <reddaly@gmail.com>
2020-05-16 12:47:11 -0700

    Introduce ListForm.

--- compile.sh
+++ compile.sh
@@ -7,4 +7,6 @@ export PATH=/home/red/git/demos/go2/bin:$PATH
 go mod tidy
 
 go tool go2go translate forms2/*.go2
+# Fix a bug with how GenericForm gets translated into sexpressions.GenericForm:
+sed -i 's/sexpressions\.//g' forms2/*.go
 go test forms2/*.go
