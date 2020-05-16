73906e701767e3c5844322c3470dc1044cd7286e
Red Daly <reddaly@gmail.com>
2020-05-16 16:31:40 -0700

    add value handling example

--- compile.sh
+++ compile.sh
@@ -10,3 +10,9 @@ go tool go2go translate forms2/*.go2
 # Fix a bug with how GenericForm gets translated into sexpressions.GenericForm:
 sed -i 's/sexpressions\.//g' forms2/*.go
 go test forms2/*.go
+
+go tool go2go translate argminmax/*.go2
+go test argminmax/*.go
+
+go tool go2go translate valueorerr/*.go2
+go test valueorerr/*.go
